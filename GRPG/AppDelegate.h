2d2bb803e255310e8d3bc0f1aae55304194a1c59
Jared Halpern <jared@byteMason.com>
2015-12-25 15:52:33 -0500

    Initial Commit

---
+++ GRPG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GRPG
+//
+//  Created by Jared Halpern on 12/25/15.
+//  Copyright © 2015 Jared Halpern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GRPG/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  GRPG
+//
+
+//  Copyright (c) 2015 Jared Halpern. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
